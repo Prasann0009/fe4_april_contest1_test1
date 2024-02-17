4bb58a246eb55d22beafb3cf8e2b0ccc1f562ff2
Prasann0009 <prasannlasod0009@gmail.com>
2024-02-17 20:28:00 +0530

    upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const SignUpForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [emailValidated, setEmailValidated] = useState(false);
+  const [passwordValidated, setPasswordValidated] = useState(false);
+  const [confirmPasswordValidated, setConfirmPasswordValidated] = useState(false);
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setEmailValidated(emailRegex.test(email));
+  };
+
+  const validatePassword = () => {
+    setPasswordValidated(password.length >= 8);
+  };
+
+  const validateConfirmPassword = () => {
+    setConfirmPasswordValidated(confirmPassword === password);
+  };
+
+  const handleSubmit = () => {
+    if (emailValidated && passwordValidated && confirmPasswordValidated) {
+      alert('Form submitted successfully');
+    } else {
+      alert('Can\'t submit the form');
+    }
+  };
+
+  return (
+    <div id="signUpForm">
+      <h2>Sign Up</h2>
+      <div>
+        <label>Email:</label><br></br>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          onBlur={validateEmail}
+          style={{ borderColor: emailValidated ? 'green' : 'red' }}
+        />
+        {!emailValidated && <div style={{ color: 'red' }}>Please enter a valid email address</div>}
+      </div>
+      <div>
+        <label>Password:</label><br></br>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          onBlur={validatePassword}
+        />
+        {!passwordValidated && <div style={{ color: 'red' }}>Password must be at least 8 characters long</div>}
+      </div>
+      <div>
+        <label>Confirm Password:</label><br></br>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          onBlur={validateConfirmPassword}
+        />
+        {!confirmPasswordValidated && <div style={{ color: 'red' }}>Passwords do not match</div>}
+      </div>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<SignUpForm/>);
+
+
